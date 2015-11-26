805d0dde5b9ee952b3496962792f25677e4b9c5f
Jure Sorn <sornjure@gmail.com>
2015-11-26 03:50:08 +0100

    Cleanup and refactore

--- README.md
+++ README.md
@@ -2,20 +2,20 @@ Odds Scraper
 ============
 
 Python scripts for scraping odds from betting websites.
+Runs on Python3 and needs BeautifulSoup4 library.
 
 How to run on…
 --------------
 
 ### Windows
 ```
->pip install BeautifulSoup
->python betbrain-scraper.py [URL/FILE]
+>pip install BeautifulSoup4
+>py betbrain-scraper.py [URL/FILE]
 ```
-In order for pip to work, python's scripts dir (C:\Python27\Scripts) needs to be in PATH.
-
+In order for pip to work, python's Scripts dir needs to be in PATH.
 
 ### UNIX
 ```
-$ sudo apt-get install python-beautifulsoup
+$ sudo apt-get install python-bs4
 $ ./betbrain-scraper.py [URL/FILE]
 ```
--- betbrain-scraper.py
+++ betbrain-scraper.py
@@ -4,39 +4,35 @@
 # Scrapes odds from passed betbrain page.
 
 import sys
-import re
-import collections
 import os
 
 from bs4 import BeautifulSoup
 import urllib.request
 from http.cookiejar import CookieJar
-from itertools import zip_longest
 
-import util
+import parser
+import printer
 
 DEFAULT_URL = 'https://www.betbrain.com/football/england/premier-league/#!/matches/'
 
-class Match:
-  def __init__(self):
-    self.bets = collections.OrderedDict()
-
-# If no arguments present it parses the default page.
+# If no arguments are present, it parses the default page.
 # Argument can be an URL or a local file.
 def main():
-  html = ""
-  if len(sys.argv) <= 1:
-    html = scrap(DEFAULT_URL)
-  elif sys.argv[1].startswith("http"):
-    html = scrap(sys.argv[1])
-  else:
-    html = readFile(sys.argv[1])
+  html = getHtml(sys.argv)
   soup = BeautifulSoup(html, "html.parser")
-  matches = getMatches(soup)
-  string = util.matchesToString(matches)
+  matches = parser.getMatches(soup)
+  string = printer.matchesToString(matches)
   print(string)
 
-# Reads webpage.
+def getHtml(argv):
+  if len(argv) <= 1:
+    return scrap(DEFAULT_URL)
+  elif argv[1].startswith("http"):
+    return scrap(argv[1])
+  else:
+    return readFile(argv[1])
+
+# Returns html file located at URL.
 def scrap(url):
   cj = CookieJar()
   opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
@@ -52,78 +48,6 @@ def readFile(path):
   except IOError:
     print((os.path.basename(__file__)+": Invalid filename."))
     sys.exit(1)
-
-# Returns dictionary of matches. Example of key/value pair:
-#   crystal-palace-fc-v-sunderland -> [[4.40, 3.75, 1.93], ...]
-def getMatches(soup):
-  matches = collections.OrderedDict()
-  bets = getBets(soup)
-  for i in range(1, 4):
-    subTab = soup.find("div", "SubTab"+str(i))
-    details = subTab.findAll("a", "MDInfo")
-    oddLists = subTab.findAll("ol", "OddsList ThreeWay")
-    for detail, oddList in zip_longest(details, oddLists, fillvalue=''):
-      addMatch(matches, detail, oddList, bets)
-  return matches
-
-# Returns list of different betting categories: ["AH", "OU", ...]
-def getBets(soup):
-  bbb = []
-  bets = soup.findAll("div", "SMItemContainer")
-  for bet in bets:
-    b = bet.find("a", "OSLink")
-    if not b:
-      b = bet.find("a", "MMHeader")
-    b = re.sub('Switch to the ', '', b['title'])
-    b = re.sub(' detailed bet type', '', b)
-    bbb.append(b)
-  return bbb
-
-# Adds match to the dictionary.
-def addMatch(matches, detail, oddList, bets):
-  name = getName(detail)
-  
-  if oddList:
-    catOdds = getSingleCategoryOdds(oddList)
-  else:
-    catOdds = ''
-  
-  if name in matches:
-    match = matches[name]
-  else:
-    match = Match()
-    matches[name] = match
-
-  bet = getBet(match, bets)
-  match.bets[bet] = catOdds
-  match.link = detail["href"]
-  match.time = getTime(detail)
-
-def getBet(match, bets):
-  for bet in bets:
-    if bet not in match.bets:
-      return bet
-
-# Returns match name in form: team1-team2-[optional time].
-def getName(detail):
-  return re.search("[^/]*/$", detail["href"]).group(0).replace('/', '')
-
-# Returns start date of the match.
-def getTime(detail):
-  return detail.find("span", "Setting DateTime").find(text=True)
-
-# Returns odds as list of lists: [4.40, 3.75, 1.93]
-def getSingleCategoryOdds(oddList):
-  odds = []
-  for oddData in oddList.findAll("li"):
-    odd = oddData.find("span", "Odds")
-    if odd:
-      odds.append(odd.find(text=True))
-      continue
-    param = oddData.find("span", "Param")
-    if param:
-      odds.append(param.find(text=True))
-  return odds
 
 if __name__ == '__main__':
   main()
